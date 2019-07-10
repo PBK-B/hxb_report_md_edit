f77f04b8ffc4b3123d1a4f1c7827ba47f31f6bcc
WangBin <wangbin@haxibiao.com>
2019-07-10 10:00:12 +0800

    初次提交

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>哈希表科技报告编辑器</title>
+</head>
+<body>
+    <a href="/edit.php?data=个人日报">个人日报</a>
+    <a href="/edit.php?data=个人周报">个人周报</a>
+</body>
+</html>
---
+++ md/demo.md.php
@@ -0,0 +1,73 @@
+# Markdown 排版工具
+
+> 根据 Github 开源项目改编搭建，日后会根据自己需求改变添加功能，很高兴你的来到
+
+## Feature
+
+- 支持常用 Markdown 标记
+- 支持代码横向滚动条
+- 支持页面主题和代码样式配置
+- 支持列表嵌套内容和列表
+
+## 常用 Markdown 标记示例
+
+### 代码示例
+
+```java
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+```
+
+### 列表示例
+
+**有序列表**
+
+1. 有序列表 one
+
+    * 嵌套列表 a
+    * 嵌套列表 b
+
+2. 有序列表 two
+
+    嵌套内容在此
+
+3. 有序列表 three
+
+**无序列表**
+
+- 无序列表 one
+
+- 无序列表 two
+
+### 表格示例
+
+| 品类 | 个数 | 备注 |
+|------|------|------|
+| 苹果 | 1    | nice |
+| 橘子 | 2    | job  |
+
+### 字体标记示例
+
+正文
+
+**加黑**
+
+*斜体*
+
+~~删除线~~
+
+### 图片示例
+
+我的大头贴
+
+![头像](https://www.zmide.com/res/logo_true.png)
+
+### 链接示例
+
+[逐梦工作室](https://www.zmide.com)
+[微信排版工具](https://md.mazhuang.org)
+
+感谢小胡子哥开源项目 GitHub 地址：<https://github.com/mzlogin/online-markdown>
