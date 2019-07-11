77e462762bad78fcc83b5c4a4584665c7d761b79
WangBin <wangbin@haxibiao.com>
2019-07-11 23:33:42 +0800

    新增产品经理日报和周报模版

---
+++ docs/md/产品经理日报.md.php
@@ -0,0 +1,33 @@
+<Logo>
+# <?php echo date('m') . '月' . date('d') . '号' ?>日报
+By：xxx
+****
+
+### 昨日工作成果
+
+- 查看并处理 xx 产品后台反馈 xx 条
+
+- 提出 xx 新功能并完善产品文档
+
+- 建设 xx 项目内容建设,发布 xx 篇文章
+
+- 参加团队建设
+相关链接：[PM 面板链接](http://code/web/haxibiao.com/commit/ba46db85874b659546c1282110627f36b19892e4)
+
+### 今日工作安排
+
+- 参加 xx 功能用户调研以及分析
+- 测试 xx 项目新功能
+- 完善 xx 项目内容建设
+
+### 问题及解决方案
+> 发现我们 xx 产品存在 xxx 问题,这里提供几条解决思路,首先要……
+
+
+### xx 项目运营数据
+![6e9fcf885b87fb9ceb7d9f9be157f0ee.png](https://i.loli.net/2019/07/11/5d274c39dd8b382959.png)
+![2044816857ab90c55f781bc33d11e56d.png](https://i.loli.net/2019/07/11/5d274c39ecaf122403.png)
+![71f258cf4f7e55de1d88f1ff3222c510.png](https://i.loli.net/2019/07/11/5d274c3a0dfe160483.png)
+
+### 运营报表图片
+![356237842e518708425277c6e98d202f.png](https://i.loli.net/2019/07/11/5d274c3a047b740805.png)
